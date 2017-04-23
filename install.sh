ef1d0b58213b80c62a170d504afc9208a45203db
belledon <mbelledonne@gmail.com>
2017-04-22 21:24:39 -0400

    Update install.sh
    For some reason the most recent push did not update this

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Pulling blender..."
 mkdir /blender-git 
 cd /blender-git 
 git clone https://git.blender.org/blender.git 
@@ -7,17 +8,19 @@ cd blender
 git submodule update --init --recursive 
 git submodule foreach git checkout master 
 git submodule foreach git pull --rebase origin master
-
+echo "Installing dependencies"
 cd ..
 rm blender/build_files/build_environment/install_deps.sh
-cp blender_fix/install_deps.sh blender/build_files/build_environment/
+cp /blender_fix/install_deps.sh blender/build_files/build_environment/
 ./install_deps.sh --source=/blender-git/
-
+echo "Building Blender..."
 cmake ../blender \
-    -DCMAKE_INSTALL_PREFIX=/opt/blender \
+    -DCMAKE_INSTALL_PREFIX=/usr/lib/python3/dist-packages \
     -DWITH_INSTALL_PORTABLE=OFF \
     -DWITH_GAMEENGINE=OFF \
     -DWITH_PYTHON_INSTALL=OFF \
     -DWITH_PLAYER=OFF \
     -DWITH_PYTHON_MODULE=ON \
     -DPYTHON_SITE_PACKAGES=/usr/lib/python3/dist-packages
+make
+make install
