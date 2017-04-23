076a84dbc7b95d8b508c04ec1f2d12541935a801
belledon <mbelledonne@gmail.com>
2017-04-22 21:25:38 -0400

    updated

--- install.sh
+++ install.sh
@@ -8,16 +8,20 @@ git submodule update --init --recursive
 git submodule foreach git checkout master 
 git submodule foreach git pull --rebase origin master
 
-cd ..
+cd /blender-git
 rm blender/build_files/build_environment/install_deps.sh
-cp blender_fix/install_deps.sh blender/build_files/build_environment/
-./install_deps.sh --source=/blender-git/
+cp /blender_fix/install_deps.sh blender/build_files/build_environment/
+echo "Y" | blender/build_files/build_environment/install_deps.sh --source=/blender-git/
 
-cmake ../blender \
-    -DCMAKE_INSTALL_PREFIX=/opt/blender \
+cd /blender-git/
+cmake blender \
+    -DCMAKE_INSTALL_PREFIX=/usr/lib/python3/dist-packages \
     -DWITH_INSTALL_PORTABLE=OFF \
     -DWITH_GAMEENGINE=OFF \
     -DWITH_PYTHON_INSTALL=OFF \
     -DWITH_PLAYER=OFF \
     -DWITH_PYTHON_MODULE=ON \
     -DPYTHON_SITE_PACKAGES=/usr/lib/python3/dist-packages
+
+make
+make install
