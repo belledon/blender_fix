9017a1ab6e215f76c69472faeb2cf0534f405533
belledon <mbelledonne@gmail.com>
2018-01-31 12:33:21 -0500

    corrected python version

--- install.sh
+++ install.sh
@@ -26,7 +26,6 @@ cmake blender \
     -DWITH_PLAYER=OFF \
     -DWITH_PYTHON_MODULE=ON \
     -DPYTHON_SITE_PACKAGES=/usr/lib/python3/dist-packages \
-    -DPYTHON_LIBRARY=/usr/bin/python3.5m \
-    -DPYTHON_INCLUDE_DIR=/usr/include/python3.5m 
+    -DPYTHON_VERSION=3.5
 make -j 4
 make install
